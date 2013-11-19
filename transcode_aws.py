cfbcc6d125a49eb7d17b6b3d3f996a2632fd8a72
Sateesh Kalidas <sateesh12@Sateeshs-MacBook-Air.local>
2013-11-19 22:05:47 +0530

    SNS notification added.

--- transcode_aws.py
+++ transcode_aws.py
@@ -54,15 +54,42 @@ def store_data_into_s3(bucket_name, key_name,file_name):
 #Note:
 #    As of now pipeline id is extracted from the web, need to find how to get it from script !
 def transcode(input_bucket, output_bucket,input_filename,output_filename):
-	transcode_input = {'Container': 'auto', 'Key': input_filename,'Resolution': 'auto', 'AspectRatio': 'auto', 'FrameRate': 'auto', 'Interlaced': 'auto'}
-	transcode_output = {'Rotate': 'auto', 'PresetId': '1351620000001-100080',  'Key': output_filename}
+#transcode_input is a set of parameters which need to be set when creating the pipeline. These are input parameters for the transcode job
+	transcode_input = {'Container': 
+			   'auto', 
+			   'Key': input_filename,
+			   'Resolution': 'auto', 
+		   	   'AspectRatio': 'auto', 
+                           'FrameRate': 'auto', 
+                           'Interlaced': 'auto'}
+#output_transcode is a set of parameters which tell the AWS, what kind of output is needed.
+#The PresetId in this case is set to HD Kindle Video codec type.
+	transcode_output = {'Rotate': 'auto', 
+                            'PresetId': '1351620000001-100080',  
+                            'Key': output_filename}
 # Connect to the elastic trans-coder.
 	et = boto.elastictranscoder.layer1.ElasticTranscoderConnection()
 	if et:
-		pipeline = et.create_pipeline("sateesh_from_script_2",input_bucket, output_bucket,"arn:aws:iam::457658703256:role/Add_ET_As_Trusted", {"Progressing":"","Completed":"","Warning":"","Error":""})
+# If you have a valid connection to AWS-ET, then create a pipeline.
+		print "Got  a valid connection to AWS-ET"
+		pipeline = et.create_pipeline("sateesh_from_script_5",input_bucket, 
+						output_bucket,
+						"arn:aws:iam::457658703256:role/Add_ET_As_Trusted", 
+						{"Progressing":"",
+						"Completed":"arn:aws:sns:us-east-1:457658703256:ABC",
+						"Warning":"",
+						"Error":""})
 		if pipeline:
-			job = et.create_job("1384084736575-ih4r4t",transcode_input,transcode_output)
+#If you get a valid pipeline object, the proceed.
+#Find the pipeline id from the name.
+			print "Pipeline creation was successful"
+			pipeline_name = find_pipeline_id_from_name("sateesh_from_script_5")
+#Now create a job for that pipeline.
+			job = et.create_job(pipeline_name,transcode_input,transcode_output)
 			if job:
+#If job is valid, then all is well, you will get an SNS notification when transcode job is completed.
+#Depending on the end point of the SNS, it may send an e-mail to your specified id, that the transcode job is complete.
+				print "Job created successfully, check for SNS end-point notification"
 				return 0
 			else:
 				return -3
@@ -98,12 +125,14 @@ def find_pipeline_id_from_name(given_pipeline_name):
                 if(pipeline_name == given_pipeline_name):
                         print "Found " + "and id is " + pipeline_id
                         return(pipeline_id)
+
+
 #Main
-retval = create_bucket("sateesh_video_transcode_primary")
-print("Debugging Message 1 : " +str(retval))
+#retval = create_bucket("sateesh_video_transcode_primary")
+#print("Debugging Message 1 : " +str(retval))
 
-retval = store_data_into_s3("sateesh_video_transcode_primary", "test","./2013-04-29 22.47.20.mp4")
-print("Debugging Message 2 : " +str(retval))
+#retval = store_data_into_s3("sateesh_video_transcode_primary", "test","./2013-04-29 22.47.20.mp4")
+#print("Debugging Message 2 : " +str(retval))
 
-retval = transcode("sateesh_video_transcode_primary","sateesh_video_transcode_primary","test","11_nov_2013_from_script")
+retval = transcode("sateesh_video_transcode_primary","sateesh_video_transcode_primary","test","19_nov_2013_from_script")
 print("Debugging Message 3 : " +str(retval))
